55889d22ee5a7a6ad38829ddd90a642822c4a9a4
Marcel Malewski <mmalewski9@gmail.com>
2022-04-24 20:25:03 +0200

    added test to GithubServiceLayerTest

--- src/test/java/com/marcel_malewski/githubapi/GithubServiceLayerTest.java
+++ src/test/java/com/marcel_malewski/githubapi/GithubServiceLayerTest.java
@@ -3,6 +3,8 @@ package com.marcel_malewski.githubapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class GithubServiceLayerTest {
 
     @Test
-    void getResponseFromLinkWhenUserNotFound() {
+    void getStatusOkWhenUserExists() throws URISyntaxException, IOException, InterruptedException {
+        GithubServiceLayer githubServiceLayer = new GithubServiceLayer();
+
+        HttpResponse<String> result = githubServiceLayer.getResponseFromLink("https://api.github.com/users/HoddityH");
+        int expectedResult = 200;
+
+        assertEquals(expectedResult, result.statusCode());
+    }
+
+    @Test
+    void getUserNotFoundWhenUserNotFound() {
         GithubServiceLayer githubServiceLayer = new GithubServiceLayer();
 
         Exception exception = assertThrows(ResponseStatusException.class, () -> {
