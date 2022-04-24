2cd366f1b935c3e4d7a5d7c1bcd51caace912094
Marcel Malewski <mmalewski9@gmail.com>
2022-04-24 19:35:55 +0200

    refactored GithubUserData

--- src/main/java/com/marcel_malewski/githubapi/github_user_data/GithubUserData.java
+++ src/main/java/com/marcel_malewski/githubapi/github_user_data/GithubUserData.java
@@ -14,4 +14,14 @@ public class GithubUserData {
     private String name;
     private String bio;
     private Map<String, Integer> languages;
+
+    public GithubUserData(String login, String name, String bio, Map<String, Integer> languages) {
+        this.login = login;
+        this.name = name;
+        this.bio = bio;
+        this.languages = languages;
+    }
+
+    public GithubUserData() {
+    }
 }
