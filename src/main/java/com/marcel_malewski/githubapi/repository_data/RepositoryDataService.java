cf3faca6c8be04e52b2638777dc7bef170fc8e02
Marcel Malewski <mmalewski9@gmail.com>
2022-04-24 12:20:07 +0200

    refactored RepositoryDataService

--- src/main/java/com/marcel_malewski/githubapi/repository_data/RepositoryDataService.java
+++ src/main/java/com/marcel_malewski/githubapi/repository_data/RepositoryDataService.java
@@ -19,7 +19,7 @@ public class RepositoryDataService extends GithubService {
 
             try {
                 //get languages as json
-                HttpResponse<String> response = getResponseFromLink(repoLanguagesUrl);
+                HttpResponse<String> response = this.getResponseFromLink(repoLanguagesUrl);
                 //convert json to map of languages
                 Map<String, Integer> mappedLanguages = this.mapper.readValue(response.body(), new TypeReference<>() {
                 });
@@ -33,35 +33,39 @@ public class RepositoryDataService extends GithubService {
         return repositoriesData;
     }
 
+    private ArrayList<RepositoryData> getAllRepositoriesData(int numberOfPages, String githubUserName) throws URISyntaxException, IOException, InterruptedException {
+        ArrayList<RepositoryData> allRepositoriesData = new ArrayList<>();
+
+        //now get every page
+        for (int i = 1; i <= numberOfPages; i++) {
+            String repositoriesUrlWithPages = String.format("https://api.github.com/users/%s/repos?page=%o&per_page=100", githubUserName, i);
+            HttpResponse<String> response = this.getResponseFromLink(repositoriesUrlWithPages);
+
+            List<RepositoryData> repositoriesData = this.mapper.readValue(response.body(), new TypeReference<>() {});
+            allRepositoriesData.addAll(repositoriesData);
+        }
+
+        return allRepositoriesData;
+    }
+
     public List<RepositoryData> getRepositoriesDataOfGithubUser(String githubUserName) throws IOException, URISyntaxException, InterruptedException {
         String githubUserInfoUrl = String.format("https://api.github.com/users/%s", githubUserName);
         //get data about githubUser as json
-        HttpResponse<String> responseWithGithubUserData = getResponseFromLink(githubUserInfoUrl);
+        HttpResponse<String> responseWithGithubUserData = this.getResponseFromLink(githubUserInfoUrl);
         //change json to object with number of public repos
         GithubUserWithNumberOfPublicRepos githubUserWithNumberOfPublicRepos = this.mapper.readValue(responseWithGithubUserData.body(), GithubUserWithNumberOfPublicRepos.class);
 
         int numberOfPages = githubUserWithNumberOfPublicRepos.getPublic_repos();
         //from github api i can get max 100 repos in one request
         if (numberOfPages > 100) {
-            ArrayList<RepositoryData> AllRepositoriesData = new ArrayList<>();
-
-            //now get every page
-            for (int i = 1; i <= numberOfPages; i++) {
-                String repositoriesUrlWithPages = String.format("https://api.github.com/users/%s/repos?page=%o&per_page=100", githubUserName, i);
-                HttpResponse<String> response = getResponseFromLink(repositoriesUrlWithPages);
-
-                List<RepositoryData> repositoriesData = this.mapper.readValue(response.body(), new TypeReference<>() {
-                });
-
-                AllRepositoriesData.addAll(repositoriesData);
-            }
+            ArrayList<RepositoryData> allRepositoriesData = getAllRepositoriesData(numberOfPages, githubUserName);
 
             //now refill languages
-            return addLanguagesToRepositoriesData(AllRepositoriesData, githubUserName);
+            return addLanguagesToRepositoriesData(allRepositoriesData, githubUserName);
         } else {
             //if number of repos is less than 100 we need only one request
             String repositoriesUrl = String.format("https://api.github.com/users/%s/repos?per_page=100", githubUserName);
-            HttpResponse<String> response = getResponseFromLink(repositoriesUrl);
+            HttpResponse<String> response = this.getResponseFromLink(repositoriesUrl);
             List<RepositoryData> repositoriesData = this.mapper.readValue(response.body(), new TypeReference<>() {});
 
             //now refill languages
