73768ce97fc33cfcbc829cd70bc57d2662053b2f
Marcel Malewski <mmalewski9@gmail.com>
2022-04-25 03:35:42 +0200

    add GithubApiApplication class

--- src/main/java/com/marcel_malewski/githubapi/GithubApiApplication.java
+++ src/main/java/com/marcel_malewski/githubapi/GithubApiApplication.java
@@ -1,17 +1,12 @@
-package com.marcel_malewski.githubapi;
+package com.marcel_malewski.githubApi;
 
-import com.marcel_malewski.githubapi.repository_data.RepositoryData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 public class GithubApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GithubApiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiApplication.class, args);
+	}
 }
